4ecf71caf2aeb97dc3df2c944a1f7c345f280df8
demiremir <d.eemirrr@gmail.com>
2022-10-16 14:28:16 +0300

    Variables

---
+++ src/hafta1/Variables.java
@@ -0,0 +1,13 @@
+package hafta1;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
