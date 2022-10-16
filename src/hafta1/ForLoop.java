97faf8cd32426863218e288557d3d03b54dca6dc
demiremir <d.eemirrr@gmail.com>
2022-10-16 15:11:19 +0300

    For loop

---
+++ src/hafta1/ForLoop.java
@@ -0,0 +1,15 @@
+package hafta1;
+
+import java.util.Iterator;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+			for (int i = 1; i < 10; i+=2) {
+				System.out.println(i);
+			}
+			System.out.println("Loop is over");
+
+	}
+
+}
