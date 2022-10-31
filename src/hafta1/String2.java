1685316b48f901a486eddd89e774e6f19889d920
demiremir <d.eemirrr@gmail.com>
2022-10-31 14:45:28 +0300

    String2

---
+++ src/hafta1/String2.java
@@ -0,0 +1,15 @@
+package hafta1;
+
+public class String2 {
+
+	public static void main(String[] args) {
+			String mesaj = "Bugün hava çok güzel";
+			System.out.println(mesaj);
+			
+			System.out.println(mesaj.replace(" ", "-"));
+			System.out.println(mesaj.substring(2));
+			System.out.println(mesaj.toLowerCase());
+			System.out.println(mesaj.toUpperCase());
+	}
+
+}
