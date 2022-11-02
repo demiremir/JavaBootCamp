23cd453653a8db9916303ab89423a2f6c46c5a3a
demiremir <d.eemirrr@gmail.com>
2022-11-02 17:39:29 +0300

    Class RecapDemo

---
+++ src/hafta2/classes/ClassRecapDemo.java
@@ -0,0 +1,15 @@
+package hafta2.classes;
+
+public class ClassRecapDemo {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		
+		int sonuc;
+		sonuc = dortIslem.Topla(3, 4);
+		
+		System.out.println(sonuc);
+
+	}
+
+}
