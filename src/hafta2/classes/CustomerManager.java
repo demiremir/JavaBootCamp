b34c0ea27069337c498b21b37d4462132c51d499
demiremir <d.eemirrr@gmail.com>
2022-11-02 16:50:38 +0300

    classes logic

--- src/hafta2/classes/CustomerManager.java
+++ src/hafta2/classes/CustomerManager.java
@@ -1,5 +1,16 @@
 package hafta2.classes;
 
 public class CustomerManager {
-
+	
+	public void Add() {
+		System.out.println("Added");
+	}
+	
+	public void Remove(){
+		System.out.println("Removed");
+	}
+	
+	public void Update() {
+		System.out.println("Updated");
+	}
 }
--- src/hafta2/classes/Main.java
+++ src/hafta2/classes/Main.java
@@ -3,8 +3,12 @@ package hafta2.classes;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CustomerManager customerManager;
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager = customerManager2;
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
 	}
 
 }
