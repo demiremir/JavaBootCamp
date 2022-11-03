960078a99346f42ffa31707b59be93ca218853b5
demiremir <d.eemirrr@gmail.com>
2022-11-03 15:52:01 +0300

    Constructor and overloading

--- src/hafta2/classes/ClassesWithAttributes.java
+++ src/hafta2/classes/ClassesWithAttributes.java
@@ -3,12 +3,12 @@ package hafta2.classes;
 public class ClassesWithAttributes {
 
 	public static void main(String[] args) {
-		ProductAttr productAttr = new ProductAttr();
-		productAttr.setName("Laptop");
-		productAttr.setId(1);
-		productAttr.setDescription("Macbook"); 
-		productAttr.setStockAmount(3);
-		productAttr.setPrice(5000);
+		ProductAttr productAttr = new ProductAttr(1, "Laptop", "Macbook", 5000, 3, "Black");
+//		productAttr.setName("Laptop");
+//		productAttr.setId(1);
+//		productAttr.setDescription("Macbook"); 
+//		productAttr.setStockAmount(3);
+//		productAttr.setPrice(5000); 
 		
 		ProductManagerAttr productManagerAttr = new ProductManagerAttr();
 		productManagerAttr.Add(productAttr);
--- src/hafta2/classes/ProductAttr.java
+++ src/hafta2/classes/ProductAttr.java
@@ -1,6 +1,22 @@
 package hafta2.classes;
 
 public class ProductAttr {
+
+	public ProductAttr(int _id, String _name, String _description, double _price, int _stockAmount, String _renk) {
+		System.out.println("Constructor calıstı.");
+		this._id = _id;
+		this._name = _name;
+		this._description = _description;
+		this._price = _price;
+		this._stockAmount = _stockAmount;
+		this._renk = _renk;
+		
+	}
+	
+	public ProductAttr() {
+		
+	}
+
 	// attribute or field
 	private int _id;
 	private String _name;
@@ -8,29 +24,36 @@ public class ProductAttr {
 	private double _price;
 	private int _stockAmount;
 	private String _renk;
-	private String _amount;
-	
-	
+	private String _kod;
+
+	public String get_kod() {
+		return _kod;
+	}
+
+	public void set_kod(String _kod) {
+		this._kod = _kod;
+	}
+
 	public int getId() {
 		return _id;
 	}
-	
+
 	public void setId(int id) {
 		_id = id;
 	}
-	
+
 	public String getName() {
 		return _name;
 	}
-	
+
 	public void setName(String name) {
 		_name = name;
 	}
-	
+
 	public String getDescription() {
 		return _description;
 	}
-	
+
 	public void setDescription(String description) {
 		_description = description;
 	}
@@ -57,14 +80,6 @@ public class ProductAttr {
 
 	public void setRenk(String renk) {
 		_renk = renk;
-	}
-
-	public String getAmount() {
-		return _amount;
-	}
-
-	public void setAmount(String amount) {
-		_amount = amount;
 	}
 
 }
